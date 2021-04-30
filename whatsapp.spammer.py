ddf68914b997dfea53f66b0d9791cb20a8851829
RASIDUL HOQUE <77561833+rasidulhoque@users.noreply.github.com>
2021-04-30 20:31:54 +0530

    Add files via upload

---
+++ whatsapp.spammer.py
@@ -0,0 +1,9 @@
+pip install pyautogui:
+import pyautogui
+import webbrowser as w
+import time
+w.open("web.whatsappp.com")
+time.sleep(50)
+for i in range(5):
+ pyautogui.press("hi")
+ pyautogui.press("enter")
