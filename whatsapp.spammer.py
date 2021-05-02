0080f96b6dea57d67ef0ea22c933177e1eee7e90
RASIDUL HOQUE <77561833+rasidulhoque@users.noreply.github.com>
2021-05-02 07:48:17 +0530

    Update whatsapp.spammer.py

--- whatsapp.spammer.py
+++ whatsapp.spammer.py
@@ -1,9 +1,12 @@
-pip install pyautogui:
+#pip install pyautogui
+
 import pyautogui
 import webbrowser as w
 import time
+
 w.open("web.whatsappp.com")
 time.sleep(50)
+
 for i in range(5):
  pyautogui.press("hi")
  pyautogui.press("enter")
